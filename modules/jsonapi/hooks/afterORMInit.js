ca102f6ff81ec2af8a37e7578ab01f38ee67f9ec
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-08-11 19:55:55 +0200

    Refresh Interval for new JSONAPI Endpoints

--- modules/jsonapi/hooks/afterORMInit.js
+++ modules/jsonapi/hooks/afterORMInit.js
@@ -4,21 +4,49 @@ module.exports = function(db$schema, c$logger, cb) {
     var Minecraft = require('minecraft');
     var jsonapi = {};
 
-    db$schema.models.JSONAPI.all(function(err, jsonapis) {
-        if(err) {
-            c$logger.error("DB Error: ", err);
-        } else {
-            jsonapis.forEach(function(jsonapiConfig) {
-                jsonapi[jsonapiConfig.name] = new Minecraft({
-                    host: jsonapiConfig.host,
-                    port: jsonapiConfig.port,
-                    user: jsonapiConfig.user,
-                    pass: jsonapiConfig.password,
-                    salt: jsonapiConfig.salt
+    function diffArray(a, b) {
+        var seen = [], diff = [];
+        for ( var i = 0; i < b.length; i++)
+            seen[b[i]] = true;
+        for ( var i = 0; i < a.length; i++)
+            if (!seen[a[i]])
+                diff.push(a[i]);
+        return diff;
+    }
+
+    function initJSONAPI() {
+        db$schema.models.JSONAPI.all(function(err, jsonapis) {
+            if(err) {
+                c$logger.error("DB Error: ", err);
+            } else {
+                var found = [];
+
+                jsonapis.forEach(function(jsonapiConfig) {
+                    found.push(jsonapiConfig.name);
+
+                    if(typeof jsonapi[jsonapiConfig.name] == "undefined") {
+                        jsonapi[jsonapiConfig.name] = new Minecraft({
+                            host: jsonapiConfig.host,
+                            port: jsonapiConfig.port,
+                            user: jsonapiConfig.user,
+                            pass: jsonapiConfig.password,
+                            salt: jsonapiConfig.salt
+                        });
+                    }
                 });
-            });
-        }
-    });
+
+                diffArray(Object.keys(jsonapi), found).forEach(function(value) {
+                    delete jsonapis[value];
+                });
+            }
+        });
+    }
+
+    initJSONAPI();
+    setInterval(function() {
+        c$logger.debug("Looking for new JSONAPI Endpoints");
+        initJSONAPI();
+    }, 30000);
 
     ModuleInjector.use("api", {
         call: function(method, args, cb) {
